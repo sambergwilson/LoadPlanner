eb57716eea44410307cf2071c758d7e5ac07d97f
Cortney Wilson <wilson84.cw@gmail.com>
2023-09-26 20:34:30 -0400

    Initial commit

---
+++ LoadPlanner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoadPlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
