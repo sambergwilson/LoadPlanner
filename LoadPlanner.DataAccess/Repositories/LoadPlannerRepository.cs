f29d137346927740b6371ddaabcd0fa3b01f6828
Cortney Wilson <wilson84.cw@gmail.com>
2023-09-28 11:16:13 -0400

    Finished Building out Repo with the functions
    Finished out building the functions in the Repo of the DataAccess side of my MVC application.

--- LoadPlanner.DataAccess/Repositories/LoadPlannerRepository.cs
+++ LoadPlanner.DataAccess/Repositories/LoadPlannerRepository.cs
@@ -16,5 +16,60 @@ namespace LoadPlanner.DataAccess.Repositories
         {
             _context = new LoadPlannerContext();
         }
+
+        // Ask about everthing here.
+        public int Create(LoadPlannerModel load)
+        {
+            _context.Add(load);
+            _context.SaveChanges();
+
+            return load.LoadID;
+        }
+
+        public int Update(LoadPlannerModel load)
+        {
+            LoadPlannerModel existingLoad = _context.LoadModel.Find(load.LoadID);
+
+            existingLoad.LoadCode = load.LoadCode;
+            existingLoad.RouteNumber = load.RouteNumber;
+            existingLoad.LocationCode = load.LocationCode;
+            existingLoad.LocationDescription = load.LocationDescription;
+            existingLoad.LocationAddress = load.LocationAddress;
+            existingLoad.DriverName = load.DriverName;
+            existingLoad.TruckCode = load.TruckCode;
+            existingLoad.IsAssigned = load.IsAssigned;
+            existingLoad.IsDeleted = load.IsDeleted;
+            existingLoad.AssignedDate = load.AssignedDate;
+            existingLoad.CompletedDate = load.CompletedDate;
+            existingLoad.IsArchived = load.IsArchived;
+
+            _context.SaveChanges();
+
+            return existingLoad.LoadID;
+        }
+
+        public bool Delete(int loadID)
+        {
+            LoadPlannerModel load = _context.LoadModel.Find(loadID);
+
+            _context.Remove(load);
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        public List<LoadPlannerModel> GetAllLoads()
+        {
+            List<LoadPlannerModel> loadList = _context.LoadModel.ToList();
+
+            return loadList;
+        }
+
+        public LoadPlannerModel GetLoadById(int loadID)
+        {
+            LoadPlannerModel loaded = _context.LoadModel.Find(loadID);
+
+            return loaded;
+        }
     }
 }
