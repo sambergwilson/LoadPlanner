57c7ad15d785a4046131b3eccba23132a547a787
Cortney Wilson <wilson84.cw@gmail.com>
2023-10-09 23:17:49 -0400

    Many attempts to Update the DeleteLoad Method
    struggling with delete load

--- LoadPlanner/wwwroot/js/site.js
+++ LoadPlanner/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function deleteLoad() {
+    var id = $("#hdnDeleteLoadId").val();
+    window.location.href = "/LoadPlanner/DeleteLoad/" + id;
+}
